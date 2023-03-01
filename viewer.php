33318182da26440a13219724cf2368c0e45100d3
K97i <105120272+K97i@users.noreply.github.com>
2023-03-01 13:17:40 +0800

    use cdn for konami js

--- viewer.php
+++ viewer.php
@@ -1530,7 +1530,7 @@ function bytesToGigabytes($bytes) {
 </body>
 <!--This should be first to make sure the themes load on time-->
 <script src="static/js/themes.js"></script>
-<script src="static/js/konami.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/konami@1.6.3/konami.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script
         src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"
