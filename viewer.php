104f6b5ca67de9739d65cd9a9aad357bdfa778ac
K97i <105120272+K97i@users.noreply.github.com>
2023-04-13 00:58:47 +0800

    remove redundant bootstrap js

--- viewer.php
+++ viewer.php
@@ -1553,7 +1553,6 @@ function getDriveCapacity($driveinput)
 
 <!--UI Stuff-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 
 <!--Main Scripts-->
 <script defer="defer" src="static/js/tables.js"></script>
