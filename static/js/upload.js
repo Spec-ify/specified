ab56d15e6b00bad6ec6c8b2d099ed46c8ab3b6d7
K97i <105120272+K97i@users.noreply.github.com>
2023-04-18 14:50:57 +0800

    remove jquery from index

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,51 +1,51 @@
 //This is the drag and drop upload handler, I wish it was easier, but it's pretty self-explanatory.
-let form = document.querySelector('form');
-//Using the jQ .on methods, we assign a set of instructions to exec at each stage of the drag event.
-$(function() {
+let form = document.querySelector("#uploadform");
+let html = document.querySelector("html");
+let text = document.querySelector("#dragtext");
+let box = document.querySelector("#uploadbox");
 
-    // Prevent Redirect
-    $("html").on("dragover", function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $("#dragtext").text("Drop it!");
-    });
-
-    $("html").on("drop", function(e) { 
-        e.preventDefault(); 
-        e.stopPropagation(); 
-    });
-
-    form.addEventListener('dragenter', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        $("#dragtext").text("Drop it!");
-        $('#uploadbox').addClass("hoverBox");
-    });
-
-    // Drag over
-    form.addEventListener('dragover', (e) => {
-            e.stopPropagation();
-            e.preventDefault();
-            $("#dragtext").text("Drop it!");
-            $('#uploadbox').addClass("hoverBox");
-        });
-
-    form.addEventListener('dragleave', (e) => {
-            $("#dragtext").text("You tease");
-            $('#uploadbox').removeClass("hoverBox");
-        });
-
-    // Drop
-    //This used to originally be an ajax call post but then SealsRock figured out a plain old submit will do the trick too, :wicked:
-    form.addEventListener('drop', (e) => {
-            document.querySelector('input').files = e.dataTransfer.files;
-            $("#uploadform").submit();
-        });
+// Prevent Redirect
+html.addEventListener("dragover", (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+	text.textContent = "Drop it!";
 });
 
+html.addEventListener("dragleave", (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+});
 
-$("html").on("drop", function(e) { e.preventDefault(); e.stopPropagation(); });
+html.addEventListener("drop", (e) => {
+	e.preventDefault();
+	e.stopPropagation();
+});
 
-$('#input-file-now').change(function(){
-    document.getElementById("uploadform").submit();
-})
+form.addEventListener("dragenter", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	box.classList.add("hoverBox");
+	text.textContent = "Drop it!";
+});
+
+form.addEventListener("dragover", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	box.classList.add("hoverBox");
+	text.textContent = "Drop it!";
+});
+
+form.addEventListener("dragleave", (e) => {
+	box.classList.remove("hoverBox");
+	text.textContent = "You tease";
+	e.stopPropagation();
+	e.preventDefault();
+});
+
+// Drop
+// Selects the input box, sets the files, and submits it
+form.addEventListener("drop", (e) => {
+	text.textContent = "Thanks! :D";
+	document.querySelector("#input-file-now").files = e.dataTransfer.files;
+	document.querySelector("#uploadform").submit();
+});
