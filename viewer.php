30176fc0a62b8c6871ce8e9cbad6a2085e9d7aed
sealsrock12 <sign@toad39.dev>
2023-09-15 16:07:00 -0700

    update bios characteristics, add cpu info

--- viewer.php
+++ viewer.php
@@ -334,7 +334,7 @@ function getDriveCapacity($driveinput)
                 <div id="main">
                     <div class="metadata_metadata expanded" id="info">
                         <div class="widgets_widgets widgets" id="hardware_widgets" data-hide="false">
-                            <div class="widget widget-cpu hover">
+                            <div class="widget widget-cpu hover" type="button" data-mdb-toggle="modal" data-mdb-target="#cpuModal">
                                 <h1>CPU</h1>
                                 <div class="widget-values">
                                     <div class="widget-value">
@@ -342,6 +342,49 @@ function getDriveCapacity($driveinput)
                                             <?= $json_data['Hardware']['Cpu']['Name'] ?>
                                         </div>
                                         <div>Callsign</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal fade " id="cpuModal" tabindex="-1" aria-labelledby="cpuModal" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="ModalLabel">CPU info</h5>
+                                            <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <table class="table">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Name</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['Name'] ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Manufacturer</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['Manufacturer'] ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Socket Designation</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['SocketDesignation'] ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Current Clock Speed</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['CurrentClockSpeed'] ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td># of Enabled Cores</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['NumberOfEnabledCore'] ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Thread Count</td>
+                                                        <td><?= $json_data['Hardware']['Cpu']['ThreadCount'] ?></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -463,7 +506,7 @@ function getDriveCapacity($driveinput)
                                 </div>
                             </div>
                             <div class="modal fade" id="boardModal" tabindex="-1" aria-labelledby="boardModal" aria-hidden="true">
-                                <div class="modal-dialog">
+                                <div class="modal-dialog modal-lg">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="ModalLabel">Board Information</h5>
