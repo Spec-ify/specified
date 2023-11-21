1f899f43d490cb2da686752c799246baf7f0a74d
zleyyij <zleyyij@noreply.users.github.com>
2023-11-21 08:22:58 -0700

    converted linkspeed from bits per second to megabits per second

--- viewer.php
+++ viewer.php
@@ -1049,7 +1049,7 @@ function getDriveCapacity($driveinput)
                                                     $table .= '
                                                             <tr>
                                                                 <td>Link Speed</td>
-                                                                <td>' . $nic["LinkSpeed"] . '</td>
+                                                                <td>' . round($nic["LinkSpeed"] * 0.00001) . 'Mbps </td>
                                                             </tr>
                                                     ';
                                                 }
