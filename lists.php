b83a6f88df76ab7d4a324cdf5ddf7477d92c81af
TheKrol <TheKrol@users.noreply.github.com>
2024-11-04 16:37:10 -0500

    Update lists.php
    Added KMSpico to the list

--- lists.php
+++ lists.php
@@ -53,6 +53,7 @@
         "Speedify", // messes with network
         "UltraUXThemePatcher", // can mess up ui
         "Lockdown Browser" // has messed up windows power plans and other things https://discord.com/channels/749314018837135390/749316166878625843/1298020309881847849
+        "KMSpico" // Known windows activation tool likely malware
     ];
 
     $biosCharacteristics = [
