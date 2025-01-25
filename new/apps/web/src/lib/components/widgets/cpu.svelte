196c213ff3eef4f37842e9c44e14bf7f002da2b7
K97i <105120272+K97i@users.noreply.github.com>
2025-01-25 11:04:13 +0800

    start genericize widgets

--- new/apps/web/src/lib/components/widgets/cpu.svelte
+++ new/apps/web/src/lib/components/widgets/cpu.svelte
@@ -1,69 +1,53 @@
 <script lang="ts">
     import { jsonData } from '../../common/access-file.js';
+    import Widget from './modal-widget.svelte';
 </script>
 
 <!-- CPU -->
-<div class="widget widget-cpu hover" type="button" data-mdb-toggle="modal" data-mdb-target="#cpu-modal">
-    <h1>CPU</h1>
-    <div class="widget-values">
-        <div class="widget-value">
-            <div class="green">
-                <!-- <?= $json_data['Hardware']['Cpu']['Name'] ?> -->
-            </div>
-            <div>Callsign</div>
+ <Widget title="CPU" modalId="cpu-modal">
+    <div slot="values">
+        <div class="green">
+            <!-- <?= $json_data['Hardware']['Cpu']['Name'] ?> -->
         </div>
+        <div>Callsign</div>
     </div>
-</div>
-<div class="modal fade " id="cpu-modal" tabindex="-1" aria-labelledby="cpu-modal" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modal-label">CPU info</h5>
-                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td>Name</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['Name'] ?></td> -->
-                        </tr>
-                        <tr>
-                            <td>Manufacturer</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['Manufacturer'] ?></td> -->
-                        </tr>
-                        <tr>
-                            <td>Socket Designation</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['SocketDesignation'] ?></td> -->
-                        </tr>
-                        <tr>
-                            <td>Current Clock Speed</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['CurrentClockSpeed'] ?></td> -->
-                        </tr>
-                        <tr>
-                            <td># of Enabled Cores</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['NumberOfEnabledCore'] ?></td> -->
-                        </tr>
-                        <tr>
-                            <td>Thread Count</td>
-                            <!-- <td><?= $json_data['Hardware']['Cpu']['ThreadCount'] ?></td> -->
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="modal-body" id="cpu-info-table" style="display:none;">
-                <!-- This content is populated javascript side -->
-                <h6 class="modal-title" id="cpu-info-title">Database results for: ...</h6>
-                <table class="table">
-                    <tbody id="fetched-cpu-info">
+  
+    <table slot="modal-body" class="table">
+        <tbody>
+            <tr>
+                <td>Name</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['Name'] ?></td> -->
+            </tr>
+            <tr>
+                <td>Manufacturer</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['Manufacturer'] ?></td> -->
+            </tr>
+            <tr>
+                <td>Socket Designation</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['SocketDesignation'] ?></td> -->
+            </tr>
+            <tr>
+                <td>Current Clock Speed</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['CurrentClockSpeed'] ?></td> -->
+            </tr>
+            <tr>
+                <td># of Enabled Cores</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['NumberOfEnabledCore'] ?></td> -->
+            </tr>
+            <tr>
+                <td>Thread Count</td>
+                <!-- <td><?= $json_data['Hardware']['Cpu']['ThreadCount'] ?></td> -->
+            </tr>
+        </tbody>
+    </table>
 
-                    </tbody>
-                </table>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" id="cpu-more-info-button">More Info</button>
-                <button type="button" class="btn btn-secondary" id="cpu-close-button" data-mdb-dismiss="modal">Close</button>
-            </div>
-        </div>
+    <div slot="extras" class="modal-body" id="cpu-info-table" style="display:none;">
+        <!-- This content is populated javascript side -->
+        <h6 class="modal-title" id="cpu-info-title">Database results for: ...</h6>
+        <table class="table">
+            <tbody id="fetched-cpu-info">
+
+            </tbody>
+        </table>
     </div>
-</div>
+</Widget>
---
+++ new/apps/web/src/lib/components/widgets/modal-widget.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let title = "Modal";
+    export let modalId = "widget-modal";
+</script>
+  
+<div class={"widget hover widget-"+ modalId} type="button" data-mdb-toggle="modal" data-mdb-target={"#" + modalId}>
+    <h1>{title}</h1>
+    <div class="widget-values">
+        <div class="widget-value">
+        <slot name="values"></slot>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id={modalId} tabindex="-1" aria-labelledby={modalId} aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id=modal-label>{title}</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <slot name="modal-body"></slot>
+            </div>
+            <div class="modal-body">
+                <slot name="extras"></slot>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" id={modalId + "-more-info-button"}>More Info</button>
+                <button type="button" class="btn btn-secondary" id={modalId + "-close-button"} data-mdb-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
