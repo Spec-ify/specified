5b3e96aee983dfaa8aedb52aad7e9085d6cabf7e
K97i <105120272+K97i@users.noreply.github.com>
2025-07-07 23:08:35 +0800

    Full GPU modal

--- new/apps/web/src/lib/components/widgets/gpu.svelte
+++ new/apps/web/src/lib/components/widgets/gpu.svelte
@@ -2,6 +2,11 @@
     import Widget from './modal-widget.svelte';
 
     export let data;
+
+    const gpuData: Record<any, Record<string, any>> = data.Hardware.Gpu;
+    const monitorData: Record<any, Record<string, any>> = data.Hardware.Monitors;
+
+    console.log(Object.values(gpuData));
 </script>
 
 <!-- GPU -->
@@ -10,88 +15,66 @@
     <div slot="values">
         <div class="widget-value">
             <div class="green">
-                <!-- <?php
-
-                if (!isset($json_data['Hardware']['Monitors'])) {
-                    echo $json_data['Hardware']['Gpu'][0]['Description'];
-                } else {
-                    echo $json_data['Hardware']['Monitors'][0]['Name'];
-                }
-
-                ?> -->
+                {#if (!monitorData)}
+                    {gpuData[0]['Description']}
+                {:else}
+                    {monitorData[0]['Name']}
+                {/if}
             </div>
             <div>Model</div>
         </div>
     </div>
 
-    <div class="modal-body">
-        <!-- <?php
-        if (isset($json_data['Hardware']['Gpu'])) {
-            $html = '<h5> GPU Info </h5>
-                                            <table class="table">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col">Name</th>
-                                                        <th scope="col">VRAM</th>
-                                                        <th scope="col">Resolution</th>
-                                                        <th scope="col">Refresh Rate</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>';
+    <div slot="modal-body" class="modal-body">
 
-            foreach ($json_data['Hardware']['Gpu'] as $gpu) {
-                $vram = $gpu['AdapterRAM'] / 1048576 . ' MB';
-                $res = $gpu['CurrentHorizontalResolution'] . ' x ' . $gpu['CurrentVerticalResolution'];
-                $refrate = $gpu['CurrentRefreshRate'] . 'Hz';
+        {#if (!(gpuData == null))}
+            <h5> GPU Info </h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">VRAM</th>
+                        <th scope="col">Resolution</th>
+                        <th scope="col">Refresh Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each Object.values(gpuData) as gpu}
+                        <tr>
+                            <td>{gpu.Description}</td>
+                            <td>{gpu.AdapterRAM / 1048576} MB</td>
+                            <td>{gpu.CurrentHorizontalResolution} x {gpu.CurrentVerticalResolution}</td>
+                            <td>{gpu.CurrentRefreshRate} Hz</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        {/if}
 
-                $html .= '<tr>
-                                                    <td>' . $gpu['Description'] . '</td>
-                                                    <td>' . $vram . '</td>
-                                                    <td>' . $res . '</td>
-                                                    <td>' . $refrate . '</td>
-                                                </tr>';
-            }
-
-            $html .= '</tbody>
-                                    </table>';
-
-            echo $html;
-
-            $html = '';
-        }
-
-        if (isset($json_data['Hardware']['Monitors'])) {
-            $html = '<h5> Monitor Info </h5>
-                                            <table class="table">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col">Name</th>
-                                                        <th scope="col">VRAM</th>
-                                                        <th scope="col">Mode</th>
-                                                        <th scope="col">Monitor</th>
-                                                        <th scope="col">Connection</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>';
-
-            foreach ($json_data['Hardware']['Monitors'] as $monitor) {
-                $html .= '<tr>
-                                                    <td>' . $monitor['Name'] . '</td>
-                                                    <td>' . $monitor['DedicatedMemory'] . '</td>
-                                                    <td>' . $monitor['CurrentMode'] . '</td>
-                                                    <td>' . $monitor['MonitorModel'] . '</td>
-                                                    <td>' . $monitor['ConnectionType'] . '</td>
-                                                </tr>';
-            }
-
-            $html .= '</tbody>
-                                    </table>';
-
-            echo $html;
-
-            $html = '';
-        }
-
-        ?> -->
+        {#if (!(monitorData == null))}
+            <h5> Monitor Info </h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">VRAM</th>
+                        <th scope="col">Mode</th>
+                        <th scope="col">Monitor</th>
+                        <th scope="col">Connection</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each Object.values(monitorData) as monitor}
+                        <tr>
+                            <td>{monitor.Name}</td>
+                            <td>{monitor.DedicatedMemory} MB</td>
+                            <td>{monitor.CurrentMode}</td>
+                            <td>{monitor.MonitorModel} Hz</td>
+                            <td>{monitor.ConnectionType} Hz</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        {/if}
     </div>
 </Widget>
