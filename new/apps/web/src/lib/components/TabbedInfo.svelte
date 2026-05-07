fba237ac9b02e9dc00f55f67cefd6f57a723d33f
K97i <105120272+K97i@users.noreply.github.com>
2026-05-07 12:46:27 +0800

    add tabbed info - notable software

--- new/apps/web/src/lib/components/TabbedInfo.svelte
+++ new/apps/web/src/lib/components/TabbedInfo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { Tabs } from "@skeletonlabs/skeleton-svelte";
 	import Notes from "./tabs/Notes.svelte";
+    import NotableSoftware from "./tabs/NotableSoftware.svelte";
 
 	export let report;
 	export let eolList;
@@ -23,7 +24,7 @@
         </Tabs.Content>
         
         <Tabs.Content value="notable-software">
-            a
+            <NotableSoftware report={report}/>
         </Tabs.Content>
 
         <Tabs.Content value="variables">
