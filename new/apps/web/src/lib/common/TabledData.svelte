195e4067aa17c97ec8d3af606e09f26dd70abce4
K97i <105120272+K97i@users.noreply.github.com>
2026-05-07 22:36:54 +0800

    Add custom table component

---
+++ new/apps/web/src/lib/common/TabledData.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+
+    import { Pagination } from '@skeletonlabs/skeleton-svelte';
+	
+	interface Props {
+		data: Array<object>;
+        headers: Array<string>;
+        params: Array<string>;
+	}
+
+	let {
+		data,
+        headers,
+        params,
+	}: Props = $props();
+
+    let search: boolean = $state(false),
+        paginate: boolean = $state(false);
+
+    let currentPage: number = $state(1),
+        pageSize: number = $state(10),
+        searchTerm: string = $state('');
+    
+    if (params.includes("search"))
+        search = true;
+
+    if (params.includes("paginate"))
+        paginate = true;
+
+    let finalData = $derived(searchTerm == "" ? data : data.filter(dataEntry => 
+            Object.values(dataEntry).some(val => 
+                String(val).toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    ))
+
+    let start = $derived((currentPage - 1) * pageSize),
+        end = $derived(start + Number(pageSize)),
+        paginatedUsers = $derived(finalData.slice(start, end));
+
+</script>
+
+<div class="table-wrap">
+    {#if params.length > 0}
+        <div class="table-options">
+            {#if paginate}
+                <label class="label">
+                    <span class="label-text">Page size</span>
+                    <select bind:value={pageSize} class="select">
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </label>
+            {/if}
+        </div>
+
+        {#if search}
+            <label class="label">
+                <span class="label-text">Search: </span>
+                <input bind:value={searchTerm} class="input" type="text" placeholder="Input" />
+            </label>
+        {/if}
+    {/if}
+    
+    <table class="table caption-bottom">
+        <thead>
+            <tr>
+                {#each headers as header}
+                    <th>{header}</th>
+                {/each}
+            </tr>
+        </thead>
+
+        <tbody class="[&>tr]:hover:preset-tonal-primary">
+            {#each paginatedUsers as dataEntry}
+                <tr>
+                    {#each headers as header}
+                        <td>{dataEntry[header]}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+
+    {#if paginate}
+        <Pagination count={finalData.length} pageSize={pageSize} {currentPage} onPageChange={(event) => (currentPage = event.page)}>
+		<Pagination.PrevTrigger>
+			<a>Previous</a>
+		</Pagination.PrevTrigger>
+		<Pagination.Context>
+			{#snippet children(pagination)}
+				{#each pagination().pages as page, index (page)}
+					{#if page.type === 'page'}
+						<Pagination.Item {...page}>
+							{page.value}
+						</Pagination.Item>
+					{:else}
+						<Pagination.Ellipsis {index}>&#8230;</Pagination.Ellipsis>
+					{/if}
+				{/each}
+			{/snippet}
+		</Pagination.Context>
+		<Pagination.NextTrigger>
+			<a onclick={console.log(end)}>Next</a>
+		</Pagination.NextTrigger>
+	</Pagination>
+    {/if}
+</div>
